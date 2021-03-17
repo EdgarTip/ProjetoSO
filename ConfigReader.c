18a02a7477fc86c4f6761408bea6cf82e21dcb46
iceD00G <pedro.iced00g.mv@gmail.com>
2021-03-17 18:51:09 +0000

    Escrita da hora para o log file. Criaçao de processos equipas (RaceManager)

--- ConfigReader.c
+++ ConfigReader.c
@@ -12,56 +12,66 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#include "RaceManager.c"
+
+
+#define DEBUG
+
 int * shared_var;
 
 
 int main(){
-  FILE *fp;
-  fp = fopen("/home/user/github/Race_Manager/configs.txt", "r"); // read mode
+  FILE *fp_configs;
+  fp_configs = fopen("/home/user/github/Race_Manager/configs.txt", "r");
 
   int time_units_per_second, lap_distance, number_of_laps, number_of_teams, T_Avaria, T_Box_Min, T_Box_Max, fuel_capacity;
-  if (fp == NULL)
+  if (fp_configs == NULL)
   {
+     #ifdef DEBUG
      perror("Error while opening the file.\n");
+     #endif
      exit(EXIT_FAILURE);
   }
+  #ifdef DEBUG
   printf("Ficheiro aberto com exito\n");
+  #endif
 
-  fscanf(fp,"%d\n%d, %d\n%d\n%d\n%d, %d\n%d",&time_units_per_second, &lap_distance, &number_of_laps,&number_of_teams, &T_Avaria, &T_Box_Min, &T_Box_Max, &fuel_capacity);
+  fscanf(fp_configs,"%d\n%d, %d\n%d\n%d\n%d, %d\n%d",&time_units_per_second, &lap_distance, &number_of_laps,&number_of_teams, &T_Avaria, &T_Box_Min, &T_Box_Max, &fuel_capacity);
+
+  #ifdef DEBUG
   printf("Número de unidades de tempo por segundo: %d\n",time_units_per_second);
   printf("Distância de cada volta, Número de voltas da corrida: %d, %d\n",lap_distance,number_of_laps);
   printf("Número de equipas: %d\n",number_of_teams);
   printf("T_Avaria: %d\n",T_Avaria);
   printf("T_Box_Min, T_Box_Max: %d, %d\n",T_Box_Min,T_Box_Max);
   printf("Capacidade do depósito de combustível: %d\n",fuel_capacity);
+  #endif
+  fclose(fp_configs);
 
-  fclose(fp);
-
-
-  // Create shared memory
-	int shmid=shmget(IPC_PRIVATE, sizeof(int), IPC_CREAT|0700);
-  if (shmid < 1){
-	 perror("Error in shmget with IPC_CREAT\n");
-	 exit(0);
-	}
-
-  // Attach shared memory
-  shared_var = (int*) shmat(shmid,NULL,0);
+  system("date|cut -c17-24 >> logs.txt");
 
   int pid=fork();
 
   if(pid==0){
-    printf("Processo main\n");
+    #ifdef DEBUG
+    printf("Simulador de Corrida.\n");
+    #endif
   }
   else{
     int pid2=fork();
     if(pid2==0){
-      printf("Gerador de Corrida.\n");
+      #ifdef DEBUG
+      printf("Gestor de Corrida criado.\n");
+      #endif
+      Race_Manager(number_of_teams);
     }
     else{
-      printf("Gerador de Avarias.\n");
+      #ifdef DEBUG
+      printf("Gestor de Avarias criado.\n");
+      #endif
+
     }
   }
-
+sleep(3);
   return 0;
 }
