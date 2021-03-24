48ec8049104a5705444fc9d271a6c74b6778259a
iceD00G <pedro.iced00g.mv@gmail.com>
2021-03-24 17:01:14 +0000

    asd

--- ConfigReader.c
+++ ConfigReader.c
@@ -13,13 +13,19 @@
 #include <sys/stat.h>
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Escrita da hora para o log file.
 #include "RaceManager.c"
 
 
 #define DEBUG
 
+<<<<<<< HEAD
 =======
 >>>>>>> Processes created
+=======
+>>>>>>> Escrita da hora para o log file.
 int * shared_var;
 
 
@@ -38,9 +44,15 @@ int main(){
   #ifdef DEBUG
   printf("Ficheiro aberto com exito\n");
   #endif
+<<<<<<< HEAD
 
   fscanf(fp_configs,"%d\n%d, %d\n%d\n%d\n%d, %d\n%d",&time_units_per_second, &lap_distance, &number_of_laps,&number_of_teams, &T_Avaria, &T_Box_Min, &T_Box_Max, &fuel_capacity);
 
+=======
+
+  fscanf(fp_configs,"%d\n%d, %d\n%d\n%d\n%d, %d\n%d",&time_units_per_second, &lap_distance, &number_of_laps,&number_of_teams, &T_Avaria, &T_Box_Min, &T_Box_Max, &fuel_capacity);
+
+>>>>>>> Escrita da hora para o log file.
   #ifdef DEBUG
   printf("Número de unidades de tempo por segundo: %d\n",time_units_per_second);
   printf("Distância de cada volta, Número de voltas da corrida: %d, %d\n",lap_distance,number_of_laps);
@@ -50,6 +62,7 @@ int main(){
   printf("Capacidade do depósito de combustível: %d\n",fuel_capacity);
   #endif
   fclose(fp_configs);
+<<<<<<< HEAD
 
   system("date|cut -c17-24 >> logs.txt");
 
@@ -73,21 +86,35 @@ int main(){
 
   // Attach shared memory
   shared_var = (int*) shmat(shmid,NULL,0);
+=======
+
+  system("date|cut -c17-24 >> logs.txt");
+>>>>>>> Escrita da hora para o log file.
 
   int pid=fork();
 
   if(pid==0){
+<<<<<<< HEAD
     printf("Processo main\n");
 >>>>>>> Processes created
+=======
+    #ifdef DEBUG
+    printf("Simulador de Corrida.\n");
+    #endif
+>>>>>>> Escrita da hora para o log file.
   }
   else{
     int pid2=fork();
     if(pid2==0){
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Escrita da hora para o log file.
       #ifdef DEBUG
       printf("Gestor de Corrida criado.\n");
       #endif
       Race_Manager(number_of_teams);
+<<<<<<< HEAD
     }
     else{
       #ifdef DEBUG
@@ -99,12 +126,21 @@ int main(){
 sleep(3);
 =======
       printf("Gerador de Corrida.\n");
+=======
+>>>>>>> Escrita da hora para o log file.
     }
     else{
-      printf("Gerador de Avarias.\n");
+      #ifdef DEBUG
+      printf("Gestor de Avarias criado.\n");
+      #endif
+
     }
   }
+<<<<<<< HEAD
 
 >>>>>>> Processes created
+=======
+sleep(3);
+>>>>>>> Escrita da hora para o log file.
   return 0;
 }
